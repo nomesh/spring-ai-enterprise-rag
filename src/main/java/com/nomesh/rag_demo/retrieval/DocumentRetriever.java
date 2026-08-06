ac1dce9b428ff699c273e74cb247e57e71358ca8
Nomesh De Silva <nomeshd@gmail.com>
2026-08-06 09:50:06 +0530

    added similarityThreshold(0.70) to make the query retrieve most close (good) result and no match found, it should return 'No Results Found|' rather for sake of returning anything.

--- src/main/java/com/nomesh/rag_demo/retrieval/DocumentRetriever.java
+++ src/main/java/com/nomesh/rag_demo/retrieval/DocumentRetriever.java
@@ -32,8 +32,20 @@ public class DocumentRetriever {
         SearchRequest request = SearchRequest.builder()
                 .query(question)
                 .topK(5)
+                .similarityThreshold(0.70)
                 .build();
 
         return vectorStore.similaritySearch(request);
     }
 }
+
+
+/*
+ * Score  | 	Interpretation
+ * -------------------------
+ * > 0.90	|  Excellent match
+ * 0.80–0.90|  Very good
+ * 0.70–0.80|  Good
+ * 0.60–0.70|  Acceptable
+ * < 0.50	|  Usually irrelevant
+ */
